103910002a07ecaf68e0a9ec20deb8d334991530
ernesteNtezirizaza <erneste.ntezirizaza@gmail.com>
2022-07-08 14:32:35 +0200

    finalised the app

--- VotingManagementSystem/frontend/src/screens/others/index.jsx
+++ VotingManagementSystem/frontend/src/screens/others/index.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Pressable, Image } from "react-native";
+import { View, Text, Pressable, SafeAreaView,ScrollView } from "react-native";
 import * as SecureStore from "expo-secure-store";
 import tw from "twrnc";
 import MyButton from "../../components/button";
 import { createVote, getCandidates, getProfile } from "../../services/auth";
 import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+import { useIsFocused } from "@react-navigation/native";
 
 const Home = ({ navigation }) => {
   const [user, setUser] = useState("");
   const [candidates, setCandidates] = useState([]);
   const [authError, setAuthError] = useState("");
+  const isFocused = useIsFocused();
 
   const getUserProfile = async () => {
     const profile = await getProfile();
@@ -22,6 +24,10 @@ const Home = ({ navigation }) => {
   useEffect(() => {
     getUserProfile();
   }, []);
+
+  useEffect(() => {
+    isFocused && getUserProfile()
+  },[isFocused]);
 
   const handleLogout = () => {
     SecureStore.deleteItemAsync("token");
@@ -43,67 +49,73 @@ const Home = ({ navigation }) => {
 
   return (
     <View style={tw`h-full flex pt-20 items-center`}>
-      <View>
-        <Text style={tw`font-bold text-xl text-center`}>
-          Welcome in Voting MS
-        </Text>
-        <Text style={tw`font-bold text-xl text-center mb-10`}>
-          {user.names}
-        </Text>
-        {candidates?.map((el) => (
-          <View key={el._id} style={tw` mb-4 w-[80]`}>
-            <Card>
-              <Card.Title
-                title={el.names}
-                subtitle={
-                  authError !== "" ? (
-                    <Text style={tw`mt-4 text-red-500 text-center`}>
-                      {authError}
-                    </Text>
-                  ) : (
-                    <Text>{el.total_votes || 0} votes</Text>
-                  )
-                }
-              />
-              <Card.Content>
-                <Paragraph>{el.missionStatement}</Paragraph>
-              </Card.Content>
-              <Card.Actions>
-                <Button
-                  onPress={() => {
-                    handleVote(el._id);
-                  }}
-                >
-                  Vote
-                </Button>
-              </Card.Actions>
-            </Card>
-          </View>
-        ))}
+      <SafeAreaView>
+        <ScrollView>
+          <View>
+            <Text style={tw`font-bold text-xl text-center`}>
+              Welcome in Voting MS
+            </Text>
+            <Text style={tw`font-bold text-xl text-center mb-10`}>
+              {user.names}
+            </Text>
+            {candidates?.map((el) => (
+              <View key={el._id} style={tw` mb-4 w-[80]`}>
+                <Card>
+                  <Card.Title
+                    title={el.names}
+                    subtitle={
+                      authError !== "" ? (
+                        <Text style={tw`mt-4 text-red-500 text-center`}>
+                          {authError}
+                        </Text>
+                      ) : (
+                        <Text>{el.total_votes || 0} votes</Text>
+                      )
+                    }
+                  />
+                  <Card.Content>
+                    <Paragraph>{el.missionStatement}</Paragraph>
+                  </Card.Content>
+                  <Card.Actions>
+                    <Button
+                      onPress={() => {
+                        handleVote(el._id);
+                      }}
+                    >
+                      Vote
+                    </Button>
+                  </Card.Actions>
+                </Card>
+              </View>
+            ))}
 
-        <View style={tw`mt-8`}>
-          <Pressable onPress={handleLogout}>
-            <MyButton
-              style={tw`bg-black text-white w-full mb-4 rounded-[10px]`}
-            >
-              LOGOUT
-            </MyButton>
-          </Pressable>
-          <Pressable
-            onPress={() => {
-              navigation.navigate("AddCandidate");
-            }}
-          >
-            {user.category === "ADMIN" ? (
-              <MyButton style={tw`bg-black text-white w-full rounded-[10px]`}>
-                Create Candidate
-              </MyButton>
-            ) : (
-              <View></View>
-            )}
-          </Pressable>
-        </View>
-      </View>
+            <View style={tw`mt-8`}>
+              <Pressable
+                onPress={() => {
+                  navigation.navigate("AddCandidate");
+                }}
+              >
+                {user.category === "ADMIN" ? (
+                  <MyButton
+                    style={tw`bg-black text-white mb-4 w-full rounded-[10px]`}
+                  >
+                    Create Candidate
+                  </MyButton>
+                ) : (
+                  <View></View>
+                )}
+              </Pressable>
+              <Pressable onPress={handleLogout}>
+                <MyButton
+                  style={tw`bg-red-500 text-white mb-4 w-full rounded-[10px]`}
+                >
+                  LOGOUT
+                </MyButton>
+              </Pressable>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 };
